4a55a7cea0ad41751640fd514ce160ca6bb09c6a
Dennis Seidel <den.seidel@gmail.com>
2019-07-10 10:32:19 +0200

    feat: add service

---
+++ storage/service/handler.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const getTenant: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
+
+export const createTenant: APIGatewayProxyHandler = async (event, _context) => {
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  }
+}
